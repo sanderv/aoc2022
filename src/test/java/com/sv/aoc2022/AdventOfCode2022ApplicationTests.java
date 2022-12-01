32c69a525c27069903d0cdfd1d8f8a7eb4fe63a0
Sander <sander@verbruggen.ws>
2022-12-01 21:32:46 +0100

    start.spring.io

---
+++ src/test/java/com/sv/aoc2022/AdventOfCode2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.aoc2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCode2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
